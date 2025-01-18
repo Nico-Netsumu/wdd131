eb64ecd96e2e42b0ebca371369f53c005b0013e0
Nico-Netsumu <nicobobytph@gmail.com>
2025-01-18 17:38:58 -0500

    Create temples.js

---
+++ scripts/temples.js
@@ -0,0 +1,26 @@
+// Blank Line
+
+const yearSpan = document.getElementById('year');
+const lastModifiedSpan = document.getElementById('lastModified');
+
+const currentYear = new Date().getFullYear();
+yearSpan.textContent = currentYear;
+
+const lastModified = document.lastModified;
+lastModifiedSpan.textContent = lastModified;
+
+// Hamburger
+const hamburgerButton = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburgerButton.addEventListener('click', () => {
+    const isHidden = menu.classList.contains('hidden');
+
+    if (isHidden) {
+        menu.classList.remove('hidden');
+        hamburgerButton.textContent = 'X';
+    } else {
+        menu.classList.add('hidden');
+        hamburgerButton.textContent = '☰';
+    }
+});
